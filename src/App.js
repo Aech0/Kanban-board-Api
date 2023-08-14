b5b2808058f94f020a209cc0ec07be94bf811c55
Vansh Narwani <77571073+Aech0@users.noreply.github.com>
2023-08-14 19:54:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/App.css';
+import KanbanBoard from './components/KanbanBoard';
+const App = () => {
+  return (
+    <main>
+        <KanbanBoard />
+    </main>
+  )
+}
+
+export default App
